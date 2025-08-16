9d5a1a074e9f127b0fbf359680658d45f660125b
Vadim Pushtaev <pushtaev.vm@gmail.com>
2025-08-16 13:54:55 +0300

    tit_for_tat_against_tit_for_tat_gives_3_n

--- Game.v
+++ Game.v
@@ -12,14 +12,10 @@ Definition Strategy := Game -> bool.
 Definition swap_game (g : Game) : Game :=
   map (fun '(b1, b2) => (b2, b1)) g.
 
-Fixpoint game_last (g : Game) : bool * (bool * bool) :=
+Definition game_last (g : Game) : bool * (bool * bool) :=
   match g with
   | [] => (false, (false, false))
-  | (b1, b2) :: g' =>
-    match g' with
-    | [] => (true, (b1, b2))
-    | (_, _) :: _ => game_last g'
-    end
+  | (b1, b2) :: g' => (true, (b1, b2))
   end.
 
 Definition game_to_string (g : Game) : string :=
@@ -28,8 +24,8 @@ Definition game_to_string (g : Game) : string :=
     | [] => (""%string, ""%string)
     | (b1, b2) :: tl =>
         let '(top, bottom) := rows tl in
-        (String.append (if b1 then "1"%string else "0"%string) top,
-         String.append (if b2 then "1"%string else "0"%string) bottom)
+        (String.append top (if b1 then "1"%string else "0"%string),
+         String.append bottom (if b2 then "1"%string else "0"%string))
     end in
   let newline := String (Ascii.ascii_of_nat 10) ""%string in
   let '(top_row, bottom_row) := rows g in
@@ -39,10 +35,10 @@ Definition add_pair '(x1, y1) '(x2, y2) : nat * nat :=
   (x1 + x2, y1 + y2).
 
 Definition game_step_result (b1 b2 : bool) : nat * nat :=
-  if b1 then
-    if b2 then (3, 3) else (5, 0)
+  if b1 then 
+    if b2 then (3, 3) else (0, 5)
   else
-    if b2 then (0, 5) else (1, 1).
+    if b2 then (5, 0) else (1, 1).
 
 Fixpoint game_result (g : Game) : nat * nat :=
   match g with
@@ -55,7 +51,7 @@ Fixpoint game_result (g : Game) : nat * nat :=
 
 
 Definition play (g : Game) (s1 s2 : Strategy) : Game :=
-  g ++ [(s1 g, s2 (swap_game g))].
+  (s1 g, s2 (swap_game g)) :: g.
 
 Fixpoint play_many (g : Game) (s1 s2 : Strategy) (n : nat) : Game :=
   match n with
@@ -68,7 +64,17 @@ Fixpoint play_many (g : Game) (s1 s2 : Strategy) (n : nat) : Game :=
 Definition st_always_true : Strategy :=
   fun (_ : Game) => true.
 
-Compute (game_to_string (play_many [] st_always_true st_always_true 10)).
+Definition st_always_false : Strategy :=
+  fun (_ : Game) => false.
+
+Definition st_tit_for_tat : Strategy :=
+  fun (g : Game) =>
+    match g with
+    | [] => true
+    | (_, b2) :: _ => b2
+    end.
+
+Compute (game_to_string (play (play_many [] st_always_true st_always_true 10) st_always_false st_always_false)).
 
 
 
@@ -94,15 +100,14 @@ Proof.
   intros g r1 r2 H.
   induction g.
   * reflexivity.
-  * destruct a. simpl in H. destruct g eqn:E.
-    + discriminate H.  
-    + destruct p. apply IHg in H. discriminate H.
+  * destruct a. simpl in H.
+    injection H. intros. discriminate H2.
 Qed. 
 
-(* game_last always yeilds tail (if any) *)
-Lemma game_last_tail :
+(* game_last always yeilds head (if any) *)
+Lemma game_last_head :
   forall (g: Game) (r1 r2: bool),
-    (game_last (g ++ [(r1, r2)])) = (true, (r1, r2)).
+    (game_last ((r1, r2) :: g)) = (true, (r1, r2)).
 Proof.
   intros.
   induction g.
@@ -113,6 +118,14 @@ Proof.
     + destruct p. simpl. simpl in IHg. apply IHg.
 Qed.
 
+(* Game last is reverted for swapped game *)
+Lemma game_last_swapped :
+  forall (g: Game) (b1 b2 b3: bool),
+    game_last g = (b1, (b2, b3)) <-> game_last (swap_game g) = (b1, (b3, b2)).
+Proof.
+Admitted.
+
+
 (* Always true strategy can yield only true *)
 Theorem always_true_yields_only_true :
   forall g : Game,
@@ -121,7 +134,7 @@ Proof.
   intros.
   unfold play.
   unfold st_always_true.
-  apply game_last_tail.
+  apply game_last_head.
 Qed.
 
 (* Play N+1 times is plaing N times and then 1 more *)
@@ -132,15 +145,40 @@ Proof.
   intros. simpl. reflexivity.
 Qed.
 
+(* Play only adds head to the game *)
+Lemma play_adds_head :
+  forall (g : Game) (s1 s2 : Strategy),
+  play g s1 s2 = (s1 g, s2 (swap_game g)) :: g.
+Proof.
+  intros.
+  unfold play.
+  reflexivity.
+Qed.
+
+(* game_result is associative to head *)
+Lemma game_result_assoc_head :
+  forall (g : Game) (s1 s2 : Strategy) (r1 r2 : bool),
+  game_result ((r1, r2) :: g) = add_pair (game_result g) (game_step_result r1 r2).
+Proof.
+  intros.
+  simpl.
+  destruct (add_pair (game_result g) (game_step_result r1 r2)).
+  destruct (game_result g).
+  reflexivity.
+Qed.
+
 (* Game result after play is like before plus the last step *)
 Lemma game_result_play :
   forall (g : Game) (s1 s2 : Strategy),
   game_result (play g s1 s2) = add_pair (game_result g) (game_step_result (s1 g) (s2 (swap_game g))).
 Proof.
   intros.
-  unfold game_result. simpl.
-
-
+  rewrite play_adds_head.
+  rewrite game_result_assoc_head.
+  * reflexivity.
+  * auto.
+  * auto.
+Qed.
 
 (* Always true against always true gives 3*n *)
 Theorem always_true_against_always_true_gives_3_n :
@@ -153,11 +191,100 @@ Proof.
     rewrite game_result_play.
     rewrite IHn.
     simpl.
-    apply (f_equal2 (@pair nat nat)).
-    + repeat rewrite Nat.add_succ_r.
-      repeat rewrite Nat.add_0_r.
-      reflexivity.
-    + repeat rewrite Nat.add_succ_r.
-      repeat rewrite Nat.add_0_r.
-      reflexivity.
+    repeat rewrite Nat.add_succ_r.
+    repeat rewrite Nat.add_0_r.
+    reflexivity.
 Qed.
+
+(* Always false against always false gives n *)
+Theorem always_false_against_always_false_gives_n :
+  forall n : nat,
+    game_result (play_many [] st_always_false st_always_false n) = (n, n).
+Proof.
+  induction n.
+  * simpl. reflexivity.
+  * rewrite play_many_N_plus_1.
+    rewrite game_result_play.
+    rewrite IHn.
+    simpl.
+    rewrite Nat.add_1_r.
+    reflexivity.
+Qed.
+
+(* Always true against false gives (0, 5*n) *)
+Theorem always_true_against_always_false_gives_0_5_n :
+  forall n : nat,
+    game_result (play_many [] st_always_true st_always_false n) = (0, 5 * n).
+Proof.
+  induction n.
+  * simpl. reflexivity.
+  * rewrite play_many_N_plus_1.
+    rewrite game_result_play.
+    rewrite IHn.
+    simpl.
+    repeat rewrite Nat.add_succ_r.
+    rewrite Nat.add_0_r.
+    reflexivity.
+Qed.
+
+(* Always false against always true gives (5*n, 0) *)
+Theorem always_false_against_always_true_gives_5_n_0 :
+  forall n : nat,
+    game_result (play_many [] st_always_false st_always_true n) = (5 * n, 0).
+Proof.
+  induction n.
+  * simpl. reflexivity.
+  * rewrite play_many_N_plus_1.
+    rewrite game_result_play.
+    rewrite IHn.
+    simpl.
+    repeat rewrite Nat.add_succ_r.
+    rewrite Nat.add_0_r.
+    reflexivity.
+Qed.
+
+(* Tit for tat returns true when head is true or none *)
+Theorem tit_for_tat_head_true :
+  forall (g: Game),
+  g = [] \/ game_last g = (true, (true, true)) \/ game_last g = (true, (false, true)) ->
+  st_tit_for_tat g = true.
+Proof.
+Admitted.
+
+(* Tit for tat against itself always yiels (true, true) *)
+Theorem tit_for_tat_against_itself_always_true :
+  forall n : nat,
+  game_last (play_many [] st_tit_for_tat st_tit_for_tat n) = (true, (true, true)) \/
+  n = 0 (* n=0 *).
+Proof.
+Admitted.
+
+(* Tit for tat againt tit for tat gives 3*n *)
+
+Theorem tit_for_tat_against_tit_for_tat_gives_3_n :
+  forall n : nat,
+    game_result (play_many [] st_tit_for_tat st_tit_for_tat n) = (3 * n, 3 * n).
+Proof.
+  induction n.
+  * simpl. reflexivity.
+  * rewrite play_many_N_plus_1.
+    rewrite game_result_play.
+    rewrite IHn.
+    replace (st_tit_for_tat (play_many [] st_tit_for_tat st_tit_for_tat n)) with (true).
+    replace (st_tit_for_tat (swap_game (play_many [] st_tit_for_tat st_tit_for_tat n))) with (true).
+    simpl.
+    repeat rewrite Nat.add_succ_r.
+    repeat rewrite Nat.add_0_r.
+    reflexivity.
+    + symmetry. apply tit_for_tat_head_true.
+      destruct (tit_for_tat_against_itself_always_true n).
+      +++ apply game_last_swapped in H. rewrite H. auto.
+      +++ rewrite H. auto.
+    + symmetry. apply tit_for_tat_head_true.
+      destruct (tit_for_tat_against_itself_always_true n).
+      +++ rewrite H. auto.
+      +++ rewrite H. auto.
+Qed.
+
+
+
